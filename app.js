7695ce92c85bbb849b3cfec97ea5bc280d552202
Huseyin Cece <hsyn.cece@gmail.com>
2024-08-29 21:40:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Selam'));
+app.get('/', (req, res) => res.send('BYM 9.Dönem AWS Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
